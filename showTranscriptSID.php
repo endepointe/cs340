d0555d96b1baaba034c4d9f888f101b429334fc1
EndePointe <endepointe@gmail.com>
2020-09-24 21:50:27 +0100

    added test file

--- showTranscriptSID.php
+++ showTranscriptSID.php
@@ -33,7 +33,7 @@ FROM GRADE_REPORT, STUDENT, COURSE, SECTION WHERE GRADE_REPORT.Student_number=ST
 
 	$result = mysqli_query($link, $query);
 	if (!$result) {
-    // die("Query to show fields from table failed");
+    die("Query to show fields from table failed");
     echo($result);
 	}
 	
