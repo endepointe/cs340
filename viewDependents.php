b14f569dc789ca95070ffd50199eec4c3d03e8eb
EndePointe <endepointe@gmail.com>
2020-11-23 01:26:39 +0000

    adding dependent info

--- viewDependents.php
+++ viewDependents.php
@@ -60,6 +60,7 @@
                                         echo "<td>" . $row['Sex'] . "</td>";
                                         echo "<td>" . $row['Bdate'] . "</td>";
                                         echo "<td>" . $row['Relationship'] . "</td>";
+                                        echo "<td>";
                                             echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
                                             echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
                                             echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
