d1fdeabcc0dc49bec4de52db4ccc1026196bd306
EndePointe <endepointe@gmail.com>
2020-12-07 10:49:34 +0000

    pw chng

--- project3/dbcon.js
+++ project3/dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit: 10,
   host: 'classmysql.engr.oregonstate.edu',
   user: 'cs340_johnsal',
-  password: '7670',
+  password: 'Dip537What3f#$',
   database: 'cs340_johnsal'
 });
 
